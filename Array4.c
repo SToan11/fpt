00f8d420f73ebf6e9708dc39a4464367cfbcee87
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-03-13 07:42:16 +0700

    Array4.c

--- Array4.c
+++ Array4.c
@@ -24,3 +24,31 @@ int main() {
 	}
 	
 }
+#include<stdio.h>
+
+int main() {
+    int i, j, n=0, m=0;
+    printf("Enter the number of rows in the array: "); 
+    scanf("%d", &n);
+    printf("Enter the number of columns in the array: "); 
+    scanf("%d", &m);
+    int a[n][m];
+    printf("Enter numbers into the array.\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("a[%d][%d]= ", i, j); 
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            a[i][j] = a[i][j] * a[i][j];
+        }
+    }
+    printf("Array after squaring each element: \n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            printf("a[%d][%d]=%d\n", i, j, a[i][j]);
+        }
+    }
+}
