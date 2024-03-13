427b7ff985196ef69e485e74d8061030c5abe0d7
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-03-13 07:43:32 +0700

    Array4.c

--- Array4.c
+++ Array4.c
@@ -1,32 +1,6 @@
 #include<stdio.h>
 
 int main() {
-	int i, j,n=0,m=0;;
-	printf ("Nhap phan tu cua mang: "); scanf ("%d",&n);
-	printf ("Nhap phan tu cua mang: "); scanf ("%d",&m);
-	int a[n][m];
-	printf ("Nhap so vao mang.\n");
-	for (i=0;i<n;i++){
-		for (j=0;j<m;j++){
-			printf ("a[%d][%d]= ",i,j); scanf ("%d",&a[i][j]);
-		}
-	}
-	for (i=0;i<n;i++){
-		for (j=0;j<m;j++){
-			a[i][j]=a[i][j]*a[i][j];
-		}
-	}
-	printf ("mang sau khi binh phuong: \n");
-	for(i=0;i<n;i++){
-	for(j=0;j<m;j++){
-	     printf ("a[%d][%d]=%d\n",i,j,a[i][j]);
-	}
-	}
-	
-}
-#include<stdio.h>
-
-int main() {
     int i, j, n=0, m=0;
     printf("Enter the number of rows in the array: "); 
     scanf("%d", &n);
