a919166bb398841cc99175500cc223dd7b1bc0a9
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-03-28 22:07:17 +0700

    Maximum calculation

--- maximum calculation.c
+++ maximum calculation.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
 
+// Function to find the maximum of three integers
 int max(int a, int b, int c){
-	int max=a;
-	if(b > max){
-		max=b;
-	}
-	if(c>max) {
-		max=c;
-	}
-	return max;
+    int max = a; // Assume a is the maximum initially
+    if (b > max) {
+        max = b; // Update max if b is greater than max
+    }
+    if (c > max) {
+        max = c; // Update max if c is greater than max
+    }
+    return max; // Return the maximum value
 }
 
 int main() {
-	printf("Tim max a, b, c \n");
-	int a, b, c;
-	int solonnhat;
-	printf("nhap a: "); scanf("%d", &a);
-	printf("nhap b: "); scanf("%d", &b);
-	printf("nhap c: "); scanf("%d", &c);
-	solonnhat = max(a, b, c);
-	printf("So lon nhat la: %d ", solonnhat);
+    printf("Find the maximum of a, b, c\n");
+    int a, b, c;
+    int maxNumber; // Variable to store the maximum value
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+    printf("Enter c: ");
+    scanf("%d", &c);
+
+    maxNumber = max(a, b, c); // Call the max function to find the maximum
+
+    printf("The maximum number is: %d", maxNumber); // Print the maximum value
+
+    return 0;
 }
